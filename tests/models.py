e7391d41e3c3dfd020b2093025dda403ad3f015e
Nicolai Brandt <root.1drone@gmail.com>
2023-01-11 11:52:52 +0100

    enforce unique constraints

--- tests/models.py
+++ tests/models.py
@@ -1,14 +1,37 @@
 from django.db import models
 
 
+class Section(models.Model):
+    parent = models.ForeignKey(
+        "self",
+        default=None,
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+        related_name="children",
+    )
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=("parent", "name"), name="unique section for parent"
+            )
+        ]
+
+    def __str__(self) -> str:
+        return self.name
+
+
 class TestCase(models.Model):
-    # TODO: make stuff unique (case_id, ...)
-    case_id = models.CharField(max_length=8)
-    section = models.CharField(max_length=255)
-    section_hierachy = models.CharField(max_length=255)
+    case_id = models.CharField(max_length=8, unique=True)
+    section = models.ForeignKey(Section, on_delete=models.SET_NULL)
     title = models.CharField(max_length=500)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.case_id
 
 
 class TestRun(models.Model):
@@ -28,6 +51,9 @@ class TestRun(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self) -> str:
+        return self.title
+
 
 class TestRunCase(models.Model):
     # TODO: unique together test_run test_case
@@ -37,14 +63,24 @@ class TestRunCase(models.Model):
     SKIPPED = "S"
     RETEST = "R"
     STATUS_CHOICES = [
-        (UNTESTED, "untested"),
-        (PASSED, "passed"),
-        (FAILED, "failed"),
-        (SKIPPED, "skipped"),
-        (RETEST, "retest"),
+        (UNTESTED, "Untested"),
+        (PASSED, "Passed"),
+        (FAILED, "Failed"),
+        (SKIPPED, "Skipped"),
+        (RETEST, "Retest"),
     ]
     test_run = models.ForeignKey(TestRun, on_delete=models.CASCADE)
     test_case = models.ForeignKey(TestCase, on_delete=models.CASCADE)
     status = models.CharField(max_length=1, choices=STATUS_CHOICES, default=UNTESTED)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=("test_run", "test_case"), name="unique testcase for run"
+            )
+        ]
+
+    def __str__(self) -> str:
+        return f"Run #{self.test_run.pk} - Case: {self.test_case.case_id}"
